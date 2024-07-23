054b83c36ba7d49abaa28722bf7aa4d7a5be7a32
Utku Enes GURSEL <uenesgursel@gmail.com>
2024-07-23 20:47:43 +0300

    halfway

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+struct Student {
+    isim: String,
+    yas: u8,
+}
+
+impl Student {
+    fn print(&self) {
+        println!("Isim: {}, Yas: {}", self.isim, self.yas)
+    }
+
+    // self
+    pub fn new(isim: String, yas: u8) -> Self {
+        Self { isim, yas }
+    }
+
+}
+
 fn main() {
-    println!("Hello, world!");
+    let utku = Student::new("Utku".into(), 25);
+    utku.print();
 }
