ae7189782c4d7fe46602d291a24bf9f4e354ee96
Aziz Emir <azizemir@tutanota.com>
2024-07-24 04:02:08 +0300

    feat: Implement 'add_bank' method for 'PaymentProcessor'

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,37 @@
 // src/main.rs
 
 mod bank;
-mod user;
 mod transaction;
+mod user;
 
 use bank::Bank;
-use user::User;
 use transaction::Transaction;
+use user::User;
 
 struct PaymentProcessor {
     banks: Vec<Bank>,
 }
 
+impl PaymentProcessor {
+    pub fn new() -> Self {
+        Self { banks: Vec::new() }
+    }
+    
+    pub fn add_bank(&mut self, bank: Bank) {
+        self.banks.push(bank);
+    }
+}
+
 fn main() {
-    //* Nothing here
+    let mut processor = PaymentProcessor::new();
+    let bank = Bank {
+        users: Vec::new(),
+        balance: 0,
+        fee: 0,
+        transactions: Vec::new(),
+    };
+
+    processor.add_bank(bank);
+
+    println!("Bank added to the payment processor.");
 }
