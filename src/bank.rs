f13831f96ca140b24aff06a48f3aed69ec132d7d
Aziz Emir <azizemir@tutanota.com>
2024-07-24 04:13:03 +0300

    feat: Add 'new' method with fee and 'add_user' method to 'Bank' struct

--- src/bank.rs
+++ src/bank.rs
@@ -1,11 +1,27 @@
 // src/bank.rs
 
-use crate::user::User;
 use crate::transaction::Transaction;
+use crate::user::User;
 
 pub struct Bank {
     pub users: Vec<User>,
     pub balance: u128,
     pub fee: u8,
     pub transactions: Vec<Transaction>,
+}
+
+impl Bank {
+    pub fn new(fee: u8) -> Self {
+        Self {
+            users: Vec::new(),
+            balance: 0,
+            fee,
+            transactions: Vec::new(),
+        }
+    }
+
+    // Method to add a user to the bank
+    pub fn add_user(&mut self, user: User) {
+        self.users.push(user);
+    }
 }
